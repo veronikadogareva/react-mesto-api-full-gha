6d8dd51cf47112ce6fdf203a019f5ea007763868
veronikadogareva <veronika.dogareva@yandex.ru>
2023-08-15 14:29:42 +0700

    rewie1

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 require('dotenv').config();
@@ -20,10 +21,17 @@ const {
 } = require('./middlewares/validate');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
+const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-
+//* * ограничитель запросов */
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Слишком много запросов, пожалуйста попробуйте позже',
+});
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -39,13 +47,13 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-app.post('/signin', loginValidation, login);
-app.post('/signup', createUserValidation, createUser);
+app.post('/signin',  authLimiter, loginValidation, login);
+app.post('/signup',  authLimiter, createUserValidation, createUser);
 app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-app.use('/', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+app.use('/*', (req, res) => {
+  next(new NotFoundError('Страница не найдена'));
 });
 app.use(errorLogger);
 app.use(errors());
